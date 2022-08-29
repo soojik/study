7a2766cf88853998239daababa5196846db7e7b5
김지수 <soo@soo-macbook.local>
2022-08-29 13:25:03 +0900

    20220827~20220829

---
+++ BOJ/src/P2146/Main.java
@@ -0,0 +1,108 @@
+package P2146;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] map;
+    static boolean[][] check;
+    static int[] dr = {0, -1, 0, 1};
+    static int[] dc = {-1, 0, 1, 0};
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/P2146/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        check = new boolean[N][N];
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!check[i][j] && map[i][j] == 1) {
+                    dfs_num(i, j, ++cnt);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] >= 1) {
+                    check = new boolean[N][N];
+                    bfs(i, j);
+                }
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    public static void dfs_num(int x, int y, int cnt) {
+        check[x][y] = true;
+        map[x][y] = cnt;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dr[i];
+            int ny = y + dc[i];
+
+            if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                if (map[nx][ny] == 1 && !check[nx][ny]) {
+                    dfs_num(nx, ny, cnt);
+                }
+            }
+        }
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<Info> q = new LinkedList<>();
+        q.add(new Info(x, y, 0));
+        check[x][y] = true;
+
+        int land = map[x][y];
+        while (!q.isEmpty()) {
+            Info curr = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = curr.x + dr[i];
+                int ny = curr.y + dc[i];
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                    if (map[nx][ny] != land && !check[nx][ny]) {
+                        if (map[nx][ny] == 0) {
+                            q.add(new Info(nx, ny, curr.cnt+1));
+                            check[nx][ny] = true;
+                        }
+                        else {
+                            min = Math.min(min, curr.cnt);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Info {
+    int x;
+    int y;
+    int cnt;
+
+    public Info (int x, int y, int cnt) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+    }
+}
---
+++ BOJ/src/P7576/Main.java
@@ -0,0 +1,85 @@
+package P7576;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static int[][] check;
+    static int[] dr = {0, -1, 0, 1};
+    static int[] dc = {-1, 0, 1, 0};
+    static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/P7576/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Queue<Tomato> q = new LinkedList<>();
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1) {
+                    q.add(new Tomato(i, j));
+                }
+            }
+        }
+
+        check = new int[M][N];
+
+        while (!q.isEmpty()) {
+            Tomato curr = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int next_x = curr.x + dr[i];
+                int next_y = curr.y + dc[i];
+
+                if (0 <= next_x && next_x < M && 0 <= next_y && next_y < N) {
+                    if (map[next_x][next_y] == 0 && check[next_x][next_y] == 0) {
+                        map[next_x][next_y] = 1;
+                        check[next_x][next_y] = check[curr.x][curr.y] + 1;
+                        q.add(new Tomato(next_x, next_y));
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                answer = Math.max(check[i][j], answer);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
+
+class Tomato {
+    int x;
+    int y;
+
+    public Tomato (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}

9f045e8b89779df16fde88bd6fd777a6c02bda2d
김지수 <wltn2858@swu.ac.kr>
2022-09-27 15:32:36 +0900

    20220927

---
+++ BOJ/src/P11605/Main.java
@@ -0,0 +1,63 @@
+package P11605;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static List<Point> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/P11605/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        list.sort(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                if (o1.getX() == o2.getX()) {
+                    return o1.getY() - o2.getY();
+                } else return o1.getX() - o2.getX();
+            }
+        });
+
+        for (Point p : list) {
+            System.out.println(p);
+        }
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+}

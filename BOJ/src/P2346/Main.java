56c65f11e362b9d1fd6651cc6126fdd29b410ee3
김지수 <wltn2858@swu.ac.kr>
2023-03-18 23:50:34 +0900

    solve BOJ - 2346

--- BOJ/src/P2346/Main.java
+++ BOJ/src/P2346/Main.java
@@ -4,54 +4,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Deque<Item> q;
     static int N;
-    public static class Item {
-        int index;
-        int next;
+    static Deque<Balloon> deque;
 
-        public Item(int index, int next) {
-            this.index = index;
-            this.next = next;
-        }
-    }
-    public static void main(String[] args) throws IOException, FileNotFoundException {
-        System.setIn(new FileInputStream("src/P2346/input.txt"));
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("BOJ/src/P2346/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // Deque의 구현체는 LinkedList, ArrayDeque가 있는데
+        // LinkedList로 구현하면 이중 연결리스트라 메모리 사용량에서 초과나서 ArrayDeque로 구현
+        deque = new ArrayDeque<>();
+
         N = Integer.parseInt(br.readLine());
-        StringTokenizer stk = new StringTokenizer(br.readLine());
 
-        q = new ArrayDeque<>();
-         for (int i=0;i<N;i++) {
-             q.add(new Item(i, Integer.parseInt(stk.nextToken())));
-         }
-
-         StringBuffer sb = new StringBuffer("");
-         Item item = new Item(0, 0);
-         // q에 하나 남아있을때까지
-        while (q.size() > 1) {
-            // 만약 item.next <= 0 -> 앞에서 뽑아옴
-            if (item.next >= 0) {
-                item = q.pollFirst();
-            }
-            // else 뒤에서 뽑아옴
-            else {
-                item = q.pollLast();
-            }
-            sb.append(item.index+1).append(" ");
-            // while 문으로 다음 item까지 차례 돌림
-            for (int i = 1; i < Math.abs(item.next); i++) {
-                if (item.next > 0) {
-                    q.addLast(q.pollFirst());
-                }
-                else {
-                    q.addFirst(q.pollLast());
-                }
-            }
-            // 만약 next >= 0 이면 앞에서
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            deque.addLast(new Balloon(i, Integer.parseInt(st.nextToken())));
         }
-        sb.append(q.pollFirst().index+1);
-        System.out.println(sb);
+
+        int[] answer = new int[N];
+        int idx_answer = 0;
+
+        while (!deque.isEmpty()) {
+            Balloon curr = deque.pollFirst();
+
+            answer[idx_answer++] = curr.index + 1;
+
+            if (idx_answer == N) {
+                StringBuilder sb = new StringBuilder();
+                for (int i : answer) {
+                    sb.append(i).append(" ");
+                }
+                System.out.println(sb);
+                return;
+            }
+
+            int jump = curr.note;
+
+            if (jump > 0) {
+                while (--jump > 0) {
+                    deque.addLast(deque.pollFirst());
+                }
+            }
+            else {
+                while (jump++ < 0) {
+                    deque.addFirst(deque.pollLast());
+                }
+            }
+        }
+    }
+}
+
+class Balloon {
+    int index;
+    int note;
+
+    public Balloon(int index, int note) {
+        this.index = index;
+        this.note = note;
     }
 }

a1cf3ba6fbf54ebf65fd88d8af01deaf11dcd7d9
soojik <38252649+soojik@users.noreply.github.com>
2023-08-18 15:41:01 +0900

    [Gold II] Title: 보석 도둑, Time: 2032 ms, Memory: 126068 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -20,6 +22,7 @@ public class Main {
       jewels[i] = new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
     }
 
+    // 보석을 무게순으로 오름차순 정렬
     Arrays.sort(jewels);
 
     int[] bags = new int[K];
@@ -28,19 +31,32 @@ public class Main {
       bags[i] = Integer.parseInt(br.readLine());
     }
 
+    // 가방도 최대 무게순으로 오름차순 정렬
     Arrays.sort(bags);
 
+    /*
+    bags(가방 배열)을 순회하며
+    jewels(보석 배열)을 동시에 순회하는데, 현재 가리키는 가방(bag)보다 작은 값들은 모두 우선순위에 넣고,
+    큰 값을 만나면 넣을 수 있는 보석순회를 멈추고 현재 우선순휘 큐에 가장 앞에 있는 보석을 꺼내 정답 변수(sum)에 더해준다.     
+     */
+    // 보석 순회할 때 쓴 포인터
     int p = 0;
     long sum = 0;
+    // 우선순위 큐는 가격에 대해 내림차순으로 정렬한다.
     PriorityQueue<Jewel> pq = new PriorityQueue<>(Comparator.comparingInt(Jewel::getV).reversed());
     for (int bag : bags) {
+      // p가 배열 범위 벗어나기 전까지
       while (p < N) {
+        // 현재 가리키는 가방보다 보석 무게가 더 크다면
         if (jewels[p].M > bag) {
+          // 넣을 수 있는 보석 탐색 멈추기
           break;
         }
+        // 보석을 넣을 수 있다면 우선순위 큐에 넣어준다.
         pq.add(jewels[p++]);
       }
 
+      // 넣을 수 있는 보석 중 가장 큰 값을 가진 보석, 가장 앞에 있는 객체 꺼내서 sum 에 더해준다. 
       if (!pq.isEmpty()) sum += pq.poll().V;
     }
 
@@ -57,6 +73,7 @@ class Jewel implements Comparable<Jewel> {
     V = v;
   }
 
+  // 무게에 대한 오름차순 
   @Override
   public int compareTo(Jewel j) {
     return this.M - j.M;

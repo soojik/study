0f5b9ca9f50bdb22f002cf3f080bffd2fe32e5ee
soojik <38252649+soojik@users.noreply.github.com>
2023-09-18 14:30:11 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.20 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,29 +1,34 @@
+import java.util.Stack;
+
 class Solution {
     public String solution(String number, int k) {
         int len = number.length();
+        StringBuilder answer = new StringBuilder();
         
-        // String number -> int[] 변환
-        int[] nums = new int[len];
-        for (int i=0;i<len;i++) nums[i] = number.charAt(i) - '0';
+        // number 에서 수를 차례로 꺼내 stack 에 넣으며 최대한 최댓값이 앞으로 오도록 한다.
+        Stack<Integer> stack = new Stack();
         
-        StringBuilder answer = new StringBuilder("");
-        
-        // 주어진 number 에서 idx(시작점) ~ 일정범위만큼 조사해 얻은 최댓값을 answer 에 계속 더하는 식으로 정답을 구한다.
-        int idx = 0;
-        // number 길이에서 k 를 뺀 만큼의 길이가 나와야하므로 탐색(시작점)도 len-k 까지만 진행한다.
-        for (int i=0;i<len-k;i++) {
-            int max = 0;
-            for (int j=idx;j<=i+k;j++) {
-                // 최댓값 업데이트 후, 다음 탐색 시작점은 현재 탐색한 곳 다음 인덱스로 지정해준다.
-                if (max < nums[j]) {
-                    max = nums[j];
-                    idx = j + 1;
-                }
+        // number 에 숫자를 순회 
+        for (char c : number.toCharArray()) {
+            // stack 에 값이 있으면서, 맨 윗 값이 현재 비교할 number 의 수보다 작으면서 숫자를 제거할 기회가 있다면
+            while (!stack.isEmpty() && stack.peek() < c - '0' && k > 0) {
+                // stack 에서 모두 뽑아내고
+                stack.pop();
+                // 제거권 감소
+                --k;
             }
-            // 최댓값 answer 에 더해준다.
-            answer.append(max);
+            
+            // 비교한 숫자는 stack 에 넣어준다.
+            stack.push(c - '0');
         }
         
-        return answer.toString();
+        // stack 에서 제거권 남은만큼 꺼내주고
+        for (int i=0;i<k;i++) stack.pop();
+        
+        // 남은 수는 모두 answer 에 넣어준다.
+        while (!stack.isEmpty()) answer.append(stack.pop());
+        
+        // 후입선출 구조이므로 reverse 후에 문자열로 변환
+        return answer.reverse().toString();
     }
 }

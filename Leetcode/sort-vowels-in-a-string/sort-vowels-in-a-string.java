dcf6f7dd7dd0e6870a101ec52d29d5e40629f466
김지수 <wltn2858@swu.ac.kr>
2023-11-14 02:15:37 +0900

    leetcode 폴더 추가

---
+++ Leetcode/sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+  public String sortVowels(String s) {
+    StringBuilder sb =new StringBuilder();
+    String vowels = "AEIOUaeiou";
+
+    PriorityQueue<String> pq = new PriorityQueue();
+    HashMap<Integer, Character> answer = new HashMap();
+
+    int len = s.length();
+
+    for (int i=0;i<len;i++) {
+      if (vowels.contains(s.charAt(i)+"")) pq.add(s.charAt(i)+"");
+      else answer.put(i, s.charAt(i));
+    }
+
+    for (int i=0;i<len;i++) {
+      if (answer.containsKey(i)) sb.append(answer.get(i));
+      else sb.append(pq.poll());
+    }
+
+    return sb.toString();
+  }
+}

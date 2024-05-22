6e8de36e248c6c6359459d52db57b64fee943c18
soojik <38252649+soojik@users.noreply.github.com>
2024-05-22 20:07:38 +0900

    [Silver III] Title: 주유소, Time: 388 ms, Memory: 35440 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -26,19 +27,18 @@ public class Main {
     }
 
     int lastPrice = prices[0]; // 마지막 주유소의 리터당 가격
-    int lastDist = dist[0]; // 마지막으로 주유한 주유소로부터의 누적 거리
+    int lastDist = 0; // 마지막으로 주유한 주유소로부터의 누적 거리
 
     long ans = 0;
     for (int i = 0; i < N; i++) {
       lastDist += dist[i]; // 이동한 거리 추가
       if (prices[i] < lastPrice) { // 만약 현 마을의 주유소 가격이 더 합리적이라면
-        ans += lastDist * lastPrice; // 마지막 충전한 주유소로부터의 거리와 그 주유소의 리터당 가격을 곱해 정산
+        ans += (long) lastDist * lastPrice; // 마지막 충전한 주유소로부터의 거리와 그 주유소의 리터당 가격을 곱해 정산
         lastPrice = prices[i]; // 현 마을의 주유소 가격으로 업데이트
         lastDist = 0; // 이동한 거리 0으로 초기화
       }
     }
-
-    ans += lastDist * lastPrice;
+    ans += (long) lastDist * lastPrice;
 
     System.out.println(ans);
   }

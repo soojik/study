3cf039874ec422263a7c909f037a48b0f7a3c866
soojik <38252649+soojik@users.noreply.github.com>
2024-07-12 17:44:26 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,19 +4,16 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         
-        HashMap<String, HashSet<String>> type_name = new HashMap();
-        
+        HashMap<String, HashSet<String>> map = new HashMap();
         for (String[] c : clothes) {
-            String type = c[1];
-            String name = c[0];
-            
-            if (!type_name.containsKey(type)) type_name.put(type, new HashSet());
-            
-            type_name.get(type).add(name);
+            HashSet<String> set = map.getOrDefault(c[1], new HashSet());
+            set.add(c[0]);
+            map.put(c[1], set);
         }
         
-        for (String key : type_name.keySet()) {
-            answer *= (type_name.get(key).size() + 1);
+        for (String key : map.keySet()) {
+            int size = map.get(key).size();
+            answer *= (size+1);
         }
         
         return answer - 1;

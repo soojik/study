70efa04580985d9a42aa9ebad45ad8930a789ae8
soojik <38252649+soojik@users.noreply.github.com>
2024-07-12 17:32:02 +0900

    [level 2] Title: 전화번호 목록, Time: 339.84 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,22 +4,19 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        Arrays.sort(phone_book);
+        Arrays.sort(phone_book, (p1, p2) -> p1.length() - p2.length());
         
-        int idx = 0; // 접두어로서 다른 단어에 붙는지 확인할 단어를 순회하기 위한 인덱스 
-        for (int i=1;i<phone_book.length;i++) {
-            // 접두어로 다른 단어에 붙는지 확인: phone_book[idx]
-            // 접두어가 있는지 확인할 대상: phone_book[i]
-            // 만약 접두어가 비교대상보다 길거나, 접두어로 들어가지 않는다면
-            if (phone_book[i].length() < phone_book[idx].length() || 
-                !phone_book[i].startsWith(phone_book[idx])) {
-                // 다음 접두어로 현재 단어(비교대상)를 지정 
-                idx = i;
-                continue;
+        HashSet<String> prev_num = new HashSet();
+
+        StringBuffer sb;
+        for (String p : phone_book) {
+            sb = new StringBuffer();
+            for (char c : p.toCharArray()) {
+                if (prev_num.contains(sb.append(c).toString())) return false;
             }
-            // 위 조건에 걸리지 않는다면 접두어가 있다는 경우니까 false
-            answer = false;
+            prev_num.add(p);
         }
+        
         return answer;
     }
 }

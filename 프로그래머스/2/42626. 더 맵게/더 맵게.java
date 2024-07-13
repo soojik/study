28b960563636d20023a6728585395ea915c38e73
soojik <38252649+soojik@users.noreply.github.com>
2024-07-14 00:51:58 +0900

    [level 2] Title: 더 맵게, Time: 1474.16 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,20 +4,22 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        PriorityQueue<Integer> pq = new PriorityQueue();
-        for (int s : scoville) pq.add(s);
+        // 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+        PriorityQueue<Integer> q = new PriorityQueue<>((i1, i2) -> i1 - i2);
+        for (int s : scoville) q.add(s);
         
-        while (2 <= pq.size()) {
-            if (K <= pq.peek()) return answer;
+        while (!q.isEmpty()) {
+            if (q.size() == 1) {
+                return q.poll() >= K ? answer : -1;
+            }
             
-            int food1 = pq.poll();
-            int food2 = pq.poll();
+            if (q.peek() >= K) return answer;
             
-            pq.add(food1 + food2 * 2);
+            int new_score = q.poll() + q.poll() * 2;
+            q.add(new_score);
             ++answer;
         }
         
-        
-        return (K <= pq.peek() ? answer : -1);
+        return answer;
     }
 }

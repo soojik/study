a8ebc47bb0429021395e6505e9f6675ba92994cc
soojik <38252649+soojik@users.noreply.github.com>
2024-07-13 15:10:11 +0900

    [level 2] Title: 프로세스, Time: 1.91 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -4,57 +4,26 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        PriorityQueue<Integer> pq = new PriorityQueue(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer i1, Integer i2) {
-                return i2 - i1;
-            }
-        });
+        // 인덱스, 우선순위
+        Queue<int[]> q = new LinkedList();
+        PriorityQueue<Integer> priors = new PriorityQueue<>((i1, i2) -> i2 - i1);
         
-        Queue<Process> general_queue = new LinkedList();
-        for (int i=0;i<priorities.length;i++) {
-            pq.add(priorities[i]);
-            general_queue.add(new Process(i, priorities[i]));
+        int len = priorities.length;
+        for (int i=0;i<len;i++) {
+            q.add(new int[]{i, priorities[i]});
+            priors.add(priorities[i]);
         }
         
-        while (!general_queue.isEmpty()) {
-            /*
-            pq에 가장 앞에 있는(우선순위가 가장 높은) 값이 현재 맨 앞에서 대기중인 프로세스의 우선순위와 같다면 빼기
-            그리고 이 프로세스의 번호(idx)가 location과 같다면 바로 answer 리턴
-            */
-            while (pq.peek() != general_queue.peek().getPriority()) {
-                general_queue.add(general_queue.poll());
-            }
-            Process process = general_queue.poll();
-            pq.poll();
+        while (!q.isEmpty()) {
             ++answer;
-            if (process.getIdx() == location) return answer;
+            while (q.peek()[1] != priors.peek()) {
+                q.add(q.poll());
+            }
+            priors.poll();
+            int[] curr = q.poll();
+            if (curr[0] == location) return answer;
         }
-        
         
         return answer;
-    }
-}
-
-class Process implements Comparable<Process> {
-    private int idx;
-    private int priority;
-    
-    public Process (int idx, int priority) {
-        this.priority = priority;
-        this.idx = idx;
-    }
-    
-    public int getIdx() {return idx;}
-    public int getPriority() {return priority;}
-    
-    @Override
-    public int compareTo(Process p) {
-        return p.priority - this.priority;
-    }
-    
-    @Override
-    public String toString() {
-        return "{idx: " + idx + ", priority: " + priority + "}";
     }
 }

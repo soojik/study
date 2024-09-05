a680a080548de2f93e158f8c4c86398bdb1f9b17
soojik <38252649+soojik@users.noreply.github.com>
2024-09-05 15:21:01 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,10 +1,7 @@
--- 보호소에 들어올 당시에는 중성화 되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물
--- 아이디와 생물 종, 이름
-SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
-FROM ANIMAL_INS I JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE (I.SEX_UPON_INTAKE NOT LIKE 'Spayed%'
-    AND I.SEX_UPON_INTAKE NOT LIKE 'Neutered%')
-    AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%' OR O.SEX_UPON_OUTCOME LIKE 'Neutered%')
--- 아이디 순
-ORDER BY I.ANIMAL_ID
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다.
+-- 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+SELECT a1.ANIMAL_ID, a1.ANIMAL_TYPE, a1.NAME--, a1.SEX_UPON_INTAKE, a2.SEX_UPON_OUTCOME
+FROM ANIMAL_INS a1 INNER JOIN ANIMAL_OUTS a2 ON a1.ANIMAL_ID = a2.ANIMAL_ID
+WHERE a1.SEX_UPON_INTAKE LIKE 'Intact%' AND (a2.SEX_UPON_OUTCOME LIKE 'Spayed%' OR a2.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY a1.ANIMAL_ID
 ;

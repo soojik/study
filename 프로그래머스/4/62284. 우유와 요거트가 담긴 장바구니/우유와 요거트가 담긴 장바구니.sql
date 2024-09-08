788ba5e4b03269ef588667b65196a72c1aac1978
soojik <38252649+soojik@users.noreply.github.com>
2024-09-08 14:49:26 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,17 +1,7 @@
--- 코드를 입력하세요
-WITH has_milk_and_yogurt AS (
-    SELECT *
-    FROM CART_PRODUCTS
-    WHERE NAME = 'Milk'
-    GROUP BY CART_ID
-    UNION
-    SELECT *
-    FROM CART_PRODUCTS
-    WHERE NAME = 'Yogurt'
-    GROUP BY CART_ID
-)
-SELECT CART_ID
-FROM has_milk_and_yogurt
-GROUP BY CART_ID HAVING 2 <= COUNT(*)
-ORDER BY CART_ID ASC
+-- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회
+SELECT DISTINCT(m.CART_ID)
+FROM (SELECT CART_ID FROM CART_PRODUCTS WHERE NAME='Milk') m
+    INNER JOIN (SELECT CART_ID FROM CART_PRODUCTS WHERE NAME='Yogurt') y
+        ON m.CART_ID = y.CART_ID
+ORDER BY m.CART_ID ASC
 ;

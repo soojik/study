852a89a54f886f657c1cc54a5509f46a3fea947e
soojik <38252649+soojik@users.noreply.github.com>
2024-10-01 16:56:00 +0900

    [Silver I] Title: 동물원, Time: 104 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N + 1];
 
-        int[] dp = new int[N];
-        
-        if (N == 1) {
-            System.out.println(3);
-            return;
-        }
-
-        dp[0] = 3;
-        dp[1] = 7;
-
-        for (int i = 2; i < N; i++) {
-            dp[i] = ((dp[i-1] * 2) % 9901 + dp[i-2]) % 9901;
-        }
-
-        System.out.println(dp[N - 1]);
+    if (N == 1) {
+      System.out.println(3);
+      return;
     }
+    arr[1] = 3;
+    arr[2] = 7;
+    for (int i = 3; i <= N; i++) {
+      arr[i] = (arr[i - 1] * 2 + arr[i - 2]) % 9901;
+    }
+
+    System.out.println(arr[N]);
+  }
 }

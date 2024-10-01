a213822b66980c953b239775f1bfd49798e50cfb
soojik <38252649+soojik@users.noreply.github.com>
2024-10-01 15:58:44 +0900

    [Silver I] Title: 점프, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -5,52 +5,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int stoi(String s) {return Integer.parseInt(s);}
-
   static int[] dr = {1, 0};
   static int[] dc = {0, 1};
-
-  static int N;
-  static int[][] board;
-  static long[][] cnt;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    N = stoi(br.readLine());
-
-    board = new int[N][N];
-    cnt = new long[N][N];
+    int N = Integer.parseInt(br.readLine());
+    int[][] map = new int[110][110];
+    long[][] dp = new long[110][110];
+    dp[0][0] = 1;
 
     StringTokenizer st;
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < N; j++) {
-        board[i][j] = stoi(st.nextToken());
+        map[i][j] = Integer.parseInt(st.nextToken());
       }
     }
 
-    cnt[0][0] = 1;
-
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
+        if (dp[i][j] == 0 || (i == N - 1 && j == N - 1)) continue;
         for (int k = 0; k < 2; k++) {
-          // 이동할 칸 수
-          int move = board[i][j];
-          // 0이면 도착지니까 현위치에서 멈추기
-          if (move == 0) continue;
-          // 다음 위치
-          int nr = i + dr[k] * move;
-          int nc = j + dc[k] * move;
-
-          // 범위 안이라면
-          if (0 <= nr && nr < N && 0 <= nc && nc < N) {
-            // 다음 경로에 현재 경우의 수 더해주기
-            cnt[nr][nc] += cnt[i][j];
-          }
+          dp[i + dr[k] * map[i][j]][j + dc[k] * map[i][j]] += dp[i][j];
         }
       }
     }
 
-    System.out.println(cnt[N-1][N-1]);
+    System.out.println(dp[N-1][N-1]);
   }
 }

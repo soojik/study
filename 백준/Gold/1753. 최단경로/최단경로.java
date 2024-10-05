3a15c343025ae3f6b8019d02ffdafd6b2225e55d
soojik <38252649+soojik@users.noreply.github.com>
2024-10-05 20:27:26 +0900

    [Gold IV] Title: 최단경로, Time: 696 ms, Memory: 109608 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,83 +2,70 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-  static int V, E, K;
-  static int[] dist;
-  static boolean[] visit;
-  static List<Node>[] graph;
-  static PriorityQueue<Node> pq = new PriorityQueue<>();
+  static int V, E;
+  static int start;
+  static int[] min;
+  static boolean[] visited;
+  static List<List<int[]>> graph = new ArrayList<>();
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
 
-    V = Integer.parseInt(st.nextToken()); // 노드 개수
-    E = Integer.parseInt(st.nextToken()); // 간선 개수
-    K = Integer.parseInt(br.readLine()); // 출발지
-
-    graph = new List[V + 1];
-    for (int i = 1; i <= V; i++) graph[i] = new ArrayList<>();
-    dist = new int[V + 1];
-    visit = new boolean[V + 1];
-    Arrays.fill(dist, Integer.MAX_VALUE);
+    start = Integer.parseInt(br.readLine());
+    min = new int[V + 1];
+    visited = new boolean[V + 1];
+    Arrays.fill(min, Integer.MAX_VALUE);
+    for (int i = 0; i <= V; i++) graph.add(new ArrayList<>());
 
     for (int i = 0; i < E; i++) {
       st = new StringTokenizer(br.readLine());
-      int u = Integer.parseInt(st.nextToken()); // 출발지
-      int v = Integer.parseInt(st.nextToken()); // 도착지
-      int w = Integer.parseInt(st.nextToken()); // 가중치
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
 
-      graph[u].add(new Node(v, w));
+      graph.get(u).add(new int[]{v, w});
     }
 
-    Dijkstra();
+    dijkstra();
 
     StringBuffer sb = new StringBuffer();
     for (int i = 1; i <= V; i++) {
-      sb.append(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]).append("\n");
+      sb.append(min[i] == Integer.MAX_VALUE ? "INF" : min[i]).append("\n");
     }
 
     System.out.println(sb);
   }
 
-  static void Dijkstra() {
-    // 시작점
+  static void dijkstra() {
+    PriorityQueue<int[]> q = new PriorityQueue<>((int[] i1, int[] i2) -> i1[1] - i2[1]);
+    q.add(new int[]{start, 0});
+    min[start] = 0;
 
-    dist[K] = 0;
-    pq.add(new Node(K, 0));
+    while (!q.isEmpty()) {
+      int[] curr = q.poll();
 
-    while (!pq.isEmpty()) {
-      Node cur = pq.poll();
+      if (visited[curr[0]]) continue;
+      visited[curr[0]] = true;
 
-      if (visit[cur.num]) continue;
-      visit[cur.num] = true;
+      for (int[] next : graph.get(curr[0])) {
+        if (min[curr[0]] + next[1] < min[next[0]]) {
+          min[next[0]] = min[curr[0]] + next[1];
 
-      for (Node n : graph[cur.num]) {
-        if (dist[cur.num] + n.cost < dist[n.num]) {
-          dist[n.num] = Math.min(dist[cur.num] + n.cost, dist[n.num]);
-
-          pq.add(new Node(n.num, dist[n.num]));
+          q.add(new int[]{next[0], min[next[0]]});
         }
       }
     }
-  }
-}
-
-class Node implements Comparable<Node> {
-
-  int num;
-  int cost;
-
-  public Node(int num, int cost) {
-    this.num = num;
-    this.cost = cost;
-  }
-
-  @Override
-  public int compareTo(Node o) {
-    return this.cost - o.cost;
   }
 }

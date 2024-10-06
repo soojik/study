d27d2efc6e9eb427ea326be6b6697e1c4e25f230
soojik <38252649+soojik@users.noreply.github.com>
2024-10-06 22:05:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 22.28 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,33 +1,33 @@
 import java.util.*;
 
 class Solution {
+    int answer = Integer.MAX_VALUE;
     boolean[][] con;
     public int solution(int n, int[][] wires) {
-        int answer = 101;
         
         con = new boolean[n + 1][n + 1];
         for (int[] w : wires) {
-            int s = w[0], e = w[1];
-            con[s][e] = con[e][s] = true;
+            con[w[0]][w[1]] = con[w[1]][w[0]] = true;
         }
         
         for (int[] w : wires) {
-            int s = w[0], e = w[1];
-            con[s][e] = con[e][s] = false;
-            int area1 = 끊기(s, n, wires);
-            int area2 = 끊기(e, n, wires);
-            con[s][e] = con[e][s] = true;
-            answer = Math.min(Math.abs(area1 - area2), answer);
+            int i1 = w[0];
+            int i2 = w[1];
+            
+            con[i1][i2] = con[i2][i1] = false;
+            int dist1 = bfs(i1, n);
+            int dist2 = bfs(i2, n);
+            answer = Math.min(answer, Math.abs(dist1 - dist2));
+            con[i1][i2] = con[i2][i1] = true;
         }
         
         return answer;
     }
     
-    public int 끊기(int start, int n, int[][] wires) {
-        int cnt = 0;
-        boolean[] visited = new boolean[n + 1];
-        
+    int bfs(int start, int n) {
         Queue<Integer> q = new LinkedList();
+        int cnt = 0;
+        boolean[] visited = new boolean[n+1];
         q.add(start);
         visited[start] = true;
         
@@ -35,7 +35,7 @@ class Solution {
             int curr = q.poll();
             ++cnt;
             
-            for (int i = 1;i <= n;i++) {
+            for (int i=1;i<=n;i++) {
                 if (visited[i] || !con[curr][i]) continue;
                 visited[i] = true;
                 q.add(i);

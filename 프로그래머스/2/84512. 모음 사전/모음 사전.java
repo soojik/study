5cba7fab93b7bf43d58cb49126684b74c877b8d1
soojik <38252649+soojik@users.noreply.github.com>
2024-10-06 22:13:26 +0900

    [level 2] Title: 모음 사전, Time: 30.50 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,29 +1,26 @@
-import java.util.*;
-
 class Solution {
     char[] alpha = {'A', 'E', 'I', 'O', 'U'};
     int answer = 0;
-    int depth = -1;
+    int depth = 0;
     public int solution(String word) {
-        find_str("", word);
         
-        
-        return answer;
+        find("", word);
+    
+        return answer - 1;
     }
     
-    public void find_str(String curr_word, String word) {
+    void find(String curr, String word) {
         ++depth;
         
-        if (curr_word.equals(word)) {
+        if (curr.equals(word)) {
             answer = depth;
             return;
         }
-        if (curr_word.length() == 5) {
-            return;
-        }
+        
+        if (curr.length() == 5) return;
         
         for (int i=0;i<5;i++) {
-            find_str(curr_word + alpha[i], word);
+            find(curr + alpha[i], word);
         }
     }
 }

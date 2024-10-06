b07dbf9b97707b4cf3d7bb4d1c9e7f1bbb796c16
soojik <38252649+soojik@users.noreply.github.com>
2024-10-06 23:30:04 +0900

    [level 3] Title: 가장 먼 노드, Time: 1627.13 ms, Memory: 629 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,42 @@
 import java.util.*;
 
 class Solution {
+    int[] dist;
+    boolean[][] con;
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
-        // 1로부터 i번째 노드까지 나오는 모든 거리
-        List<Integer> distanceFrom1 = new ArrayList();
-        
-        List<List<Integer>> connections = new ArrayList();
-        
-        for (int i=0;i<=n;i++) connections.add(new ArrayList());
-        
+        dist = new int[n + 1];
+        con = new boolean[n+1][n+1];
         for (int[] e : edge) {
-            connections.get(e[0]).add(e[1]);
-            connections.get(e[1]).add(e[0]);
+            con[e[0]][e[1]] = con[e[1]][e[0]] = true;
         }
         
-        boolean[] visit = new boolean[n+1];
-        Queue<int[]> q = new LinkedList();
-        q.add(new int[]{1, 0});
-        visit[1] = true;
+        bfs(n);
         
-        while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            
-            int node = tmp[0];
-            int cnt = tmp[1];
-            
-            // node까지 거리
-            distanceFrom1.add(cnt);
-            
-            // 연결되어있고 아직 방문 안한 노드에 대해
-            for (int next : connections.get(node)) {
-                if (connections.get(node).contains(next) && !visit[next]) {
-                    // 방문 체크 해주고
-                    visit[next] = true;
-                    // 계속 탐색
-                    q.add(new int[]{next, cnt+1});
-                }
-            }
-        }
+        int max = 0;
+        for (int d : dist) max = Math.max(d, max);
+        for (int d : dist) if (max == d) ++answer;
         
-        // 나올 수 있는 거리 중 가장 큰 값
-        int max = Collections.max(distanceFrom1);
-        
-        // 이 몇번 나왔는지 반환
-        return Collections.frequency(distanceFrom1, max);
-        
+        return answer;
     }
     
+    void bfs(int n) {
+        Queue<int[]> q = new LinkedList();
+        boolean[] visited = new boolean[n+1];
+        q.add(new int[]{1, 0});
+        visited[1] = true;
+        
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            
+            dist[curr[0]] = Math.max(dist[curr[0]], curr[1]);
+            
+            for (int i=2;i<=n;i++) {
+                if (visited[i] || !con[curr[0]][i]) continue;
+                visited[i] = true;
+                q.add(new int[]{i, curr[1] + 1});
+            }
+        }
+    }
 }

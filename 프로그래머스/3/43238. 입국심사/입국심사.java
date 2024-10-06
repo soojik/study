ddf8b870119163b3f3988a60cb10da4adc84e1a9
soojik <38252649+soojik@users.noreply.github.com>
2024-10-07 00:47:10 +0900

    [level 3] Title: 입국심사, Time: 56.52 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,29 @@
-import java.util.Arrays;
-
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
+        long answer = 0;
         
-        // 예상 최소 소요 시간 중 최소 값과 최대 값
-        long min = 1;
-        long max = (long) times[0] * n;
+        int max = 0;
+        for (int t : times) max = Math.max(max, t);
         
-        while (min < max) {
-            // 예상 총 소요 시간
-            long mid = (min + max) / 2;
-            // mid가 총 소요시간일때 심사받을 수 있는 사람 수
-            long 입국심사받는사람 = 0;
+        long start = 1;
+        long end = max * (long)n;
+        
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long people = 0;
             
-            // times 배열 돌면서 입국심사받는사람 계산
-            for (int t : times) {
-                입국심사받는사람 += mid / t;
+            for (long t : times) {
+                people += mid / t;
             }
             
-            // 만약 목표하는 수(n)보다 입국심사받는사람이 더 크거나 같다면 더 최소값 찾을 때까지
-            if (n <= 입국심사받는사람) max = mid;
-            // 아니면 시간을 늘려야하니까
-            else min = mid + 1;
+            if (n <= people) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
         }
         
-        return min;
+        return start;
     }
 }

0f10073c816ef2da5dc3a8135145aefeb5616028
soojik <38252649+soojik@users.noreply.github.com>
2024-11-06 15:14:30 +0900

    [level 3] Title: 등굣길, Time: 0.61 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,26 +1,25 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
+        boolean[][] puddle = new boolean[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
         
-        boolean[][] isPuddle = new boolean[n+1][m+1];
         for (int[] p : puddles) {
-            isPuddle[p[1]][p[0]] = true;
+            puddle[p[1]][p[0]] = true;
         }
         
-        int[][] dp = new int[n+1][m+1];
+        for (int i=1;i<=n;i++) {
+            if (puddle[i][1]) break;
+            dp[i][1] = 1;
+        }
         
         for (int i=1;i<=m;i++) {
-            if (isPuddle[1][i]) break;
+            if (puddle[1][i]) break;
             dp[1][i] = 1;
         }
-        for (int i=1;i<=n;i++){
-            if (isPuddle[i][1]) break;
-            dp[i][1] = 1;
-        } 
         
         for (int i=2;i<=n;i++) {
             for (int j=2;j<=m;j++) {
-                if (isPuddle[i][j]) continue;
+                if (puddle[i][j]) continue;
                 dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007;
             }
         }

01d4097d03c7c846ff4ccdbbc1ec65ab8d8d9a21
soojik <38252649+soojik@users.noreply.github.com>
2024-11-08 12:25:56 +0900

    [level 3] Title: 합승 택시 요금, Time: 72.63 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -6,42 +6,29 @@ class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = Integer.MAX_VALUE;
         
-        for (int i=0;i<=n;i++) graph.add(new ArrayList<int[]>());
+        int[][] dist = new int[n+1][n+1];
+        for (int i=0;i<=n;i++) {
+            Arrays.fill(dist[i], 1000001);
+            dist[i][i] = 0;
+        }
         
         for (int[] f : fares) {
-            graph.get(f[0]).add(new int[]{f[1], f[2]});
-            graph.get(f[1]).add(new int[]{f[0], f[2]});
+            dist[f[0]][f[1]] = f[2];
+            dist[f[1]][f[0]] = f[2];
         }
         
-        int[] dist_s = 최단거리(n, s);
-        int[] dist_a = 최단거리(n, a);
-        int[] dist_b = 최단거리(n, b);
-        
-        for (int i=1;i<=n;i++) {
-            answer = Math.min(answer, dist_s[i] + dist_a[i] + dist_b[i]);
-        }
-        
-        return answer;
-    }
-    
-    int[] 최단거리(int n, int start) {
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        pq.add(new int[]{start, 0});
-        dist[start] = 0;
-        
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            
-            for (int[] next : graph.get(curr[0])) {
-                if (dist[curr[0]] + next[1] < dist[next[0]]) {
-                    dist[next[0]] = dist[curr[0]] + next[1];
-                    pq.add(new int[]{next[0], dist[next[0]]});
+        for (int k=1;k<=n;k++) {
+            for (int i=1;i<=n;i++) {
+                for (int j=1;j<=n;j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
         
-        return dist;
+        for (int i=1;i<=n;i++) {
+            answer = Math.min(answer, dist[s][i] + dist[i][a] + dist[i][b]);
+        }
+        
+        return answer;
     }
 }

c4a8da241c33882e32a662fece0dc934aa1b3014
soojik <38252649+soojik@users.noreply.github.com>
2024-11-09 17:44:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.69 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,42 +1,35 @@
 import java.util.*;
 
 class Solution {
-    List<List<int[]>> graph = new ArrayList();
-    int answer = Integer.MAX_VALUE;
+    int[] parent;
     public int solution(int n, int[][] costs) {
+        parent = new int[n];
         
-        for (int i=0;i<=n;i++) graph.add(new ArrayList<int[]>());
+        for (int i=0;i<n;i++) parent[i] = i;
         
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        
+        int answer = 0;
         for (int[] c : costs) {
-            graph.get(c[0]).add(new int[]{c[1], c[2]});
-            graph.get(c[1]).add(new int[]{c[0], c[2]});
-        }
-        
-        return 최단거리(n);
-    }
-    
-    int 최단거리(int n) {
-        Queue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
-        boolean[] visited = new boolean[n];
-        pq.add(new int[]{0, 0});
-        
-        int cnt = 0;
-        int sum = 0;
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            
-            if (visited[curr[0]]) continue;
-            
-            visited[curr[0]] = true;
-            ++cnt;
-            sum += curr[1];
-            
-            for (int[] next : graph.get(curr[0])) {
-                if (visited[next[0]]) continue;
-                pq.add(next);
+            if (find(c[0]) != find(c[1])) {
+                union(c[0], c[1]);
+                answer += c[2];
             }
         }
         
-        return sum;
+        return answer;
+    }
+    
+    int find(int a) {
+        if (parent[a] == a) return a;
+        return find(parent[a]);
+    }
+    
+    void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if (a == b) return;
+        parent[b] = a;
     }
 }

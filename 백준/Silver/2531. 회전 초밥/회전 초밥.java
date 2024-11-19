3c797ba719ffc0895a53947f63d5e7025804a14a
soojik <38252649+soojik@users.noreply.github.com>
2024-11-19 12:38:25 +0900

    [Silver I] Title: 회전 초밥, Time: 240 ms, Memory: 22520 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,43 +1,48 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); // 전체 초밥 수
-		int d = Integer.parseInt(st.nextToken()); // 초밥 가짓 수
-		int k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시 수
-		int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
+    int N, D, K, C;
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    // 접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
+    N = Integer.parseInt(st.nextToken());
+    D = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+    C = Integer.parseInt(st.nextToken());
 
-		int[] dishes = new int[N];
-		for (int i=0;i<N;i++) {
-			dishes[i] = Integer.parseInt(br.readLine());
-		}
+    int[] sushies = new int[N];
+    for(int i=0; i<N; i++) {
+      sushies[i] = Integer.parseInt(br.readLine());
+    }
 
-		int max = 0;
-		for (int i=0;i<N;i++) {
-			HashSet<Integer> set = new HashSet();
-			
-			for (int p1=i;p1<Math.min(N, i+k);p1++) {
-				set.add(dishes[p1]);
-			}
-			for (int p2=0;p2<i+k - N;p2++) {
-				set.add(dishes[p2]);
-			}
-			
-			set.add(c);
-			max = Math.max(max, set.size());
-		}
-		
-		System.out.println(max);
-	}
+    int max = 0;
+
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < K; i++) {
+      map.put(sushies[i], map.getOrDefault(sushies[i], 0) + 1);
+    }
+
+    int p = K;
+    while (p != K - 1) {
+      max = Math.max(max, map.containsKey(C) ? map.size() : map.size() + 1);
+      int idx = p - K < 0 ? p - K + N : p - K;
+      map.put(sushies[idx], map.get(sushies[idx]) - 1);
+      if (map.get(sushies[idx]) == 0) map.remove(sushies[idx]);
+      map.put(sushies[p], map.getOrDefault(sushies[p], 0) + 1);
+
+      ++p;
+      p = N <= p ? p - N : p;
+    }
+
+    System.out.println(Math.max(max, map.containsKey(C) ? map.size() : map.size() + 1));
+  }
 }

cc09c5c1869619b5a27a1c8a0a84e1e7642b6e2c
soojik <38252649+soojik@users.noreply.github.com>
2024-11-20 15:57:24 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 189.27 ms, Memory: 217 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int N = numbers.length;
+        
+        int[] answer = new int[N];
+        
+        Stack<Integer> stack = new Stack();
+        for (int i=N-1;i>=0;i--) {
+            if (stack.isEmpty()) {
+                answer[i] = -1;
+                stack.push(numbers[i]);
+                continue;
+            }
+            
+            while (!stack.isEmpty() && stack.peek() <= numbers[i]) {
+                stack.pop();
+            }
+            
+            answer[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(numbers[i]);
+        }
+        
+        return answer;
+    }
+}

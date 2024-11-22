a30260040749c74f49cba0f6a9ac46cf2fb1ba0f
soojik <38252649+soojik@users.noreply.github.com>
2024-11-22 16:13:51 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 324 ms, Memory: 33428 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,51 +1,46 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, K;
-	static int[] cnt = new int[100001]; // cnt[idx] == idx가 몇번 나왔는지
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] nums = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i=0;i<N;i++) nums[i] = Integer.parseInt(st.nextToken());
-		
-		if (N == 1) {
-			System.out.println(1);
-			return;
-		}
+    int N, K;
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
 
-		int p1 = 0, p2 = 0;
-		++cnt[nums[p1]];
-		
-		int ans = 0;
-		while (p1 <= p2 && p2 < N) {
-			if (cnt[nums[p2]] <= K) {
-				++p2;
-				if (p2 == N) {
-					ans = Math.max(ans, p2 - p1);
-					break;
-				}
-				++cnt[nums[p2]];
-			}
-			else {
-				ans = Math.max(ans, p2 - p1);
-				while (K < cnt[nums[p2]]) {
-					--cnt[nums[p1++]];
-				}
-			}
-		}
-		
-		System.out.println(ans);
-	}
-	
+    int[] numbers = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      numbers[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int p1 = 0, p2 = 0;
+    int max = 0;
+
+    int[] cnt = new int[200001];
+    ++cnt[numbers[p1]];
+
+    while (p1 <= p2) {
+      if (cnt[numbers[p2]] <= K) {
+        ++p2;
+        if (p2 == N) {
+          max = Math.max(max, p2 - p1);
+          break;
+        }
+        ++cnt[numbers[p2]];
+      }
+
+      max = Math.max(max, p2 - p1);
+      while (K < cnt[numbers[p2]]) {
+        --cnt[numbers[p1++]];
+      }
+    }
+
+    System.out.println(max);
+  }
 }
